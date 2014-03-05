b9f13e21b5aa4e453d7826c7170817334695fd2d
Abdulrahman alrumaithi <Andulrahman@abdulrahmansmbp.mshome.net>
2014-03-05 16:15:48 +0400

    add test class

---
+++ GetTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GetTest
+//
+//  Created by Abdulrahman alrumaithi on 3/5/14.
+//  Copyright (c) 2014 Abdulrahman alrumaithi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GetTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GetTest
+//
+//  Created by Abdulrahman alrumaithi on 3/5/14.
+//  Copyright (c) 2014 Abdulrahman alrumaithi. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
